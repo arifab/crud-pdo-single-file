3cb5362c3333710e48826cd3adc871135aa21834
Arif AB <arifab@users.noreply.github.com>
2020-06-13 15:22:18 +0700

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -2,12 +2,19 @@
 $DB_HOST = 'localhost';
 $DB_USER = 'root';
 $DB_PASS = 'mysql';
-$DB_NAME = 'codingcage.com';
+$DB_NAME = 'userDb';
 
+//--- DB Connection
 try{
 	$DB_con = new PDO("mysql:host={$DB_HOST};dbname={$DB_NAME}",$DB_USER,$DB_PASS);
 	$DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e){
 	echo $e->getMessage();
 }
- ?>
+
+//--- Function redirect
+function GoToNow ($url){
+	echo '<script type="text/javascript">setTimeout(function(){window.top.location="'.$url.'"} , 500);</script>';
+}
+
+?>
